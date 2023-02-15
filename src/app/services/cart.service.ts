91c8d4e564d59c4236b5ae1b4f668bae8981ba37
Keamo Mfoloe <keamogetswemfoloe@gmail.com>
2023-02-15 10:58:16 +0200

    Feat: cart service for handling cart logic

---
+++ src/app/services/cart.service.ts
@@ -0,0 +1,61 @@
+import { Injectable } from '@angular/core';
+import { Product } from '../product';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CartService {
+  items: Product[] = [];
+
+
+  addToCart(product: Product) {
+    const productExistsInCart = this.items.find(item => item.id === product.id);
+    if (productExistsInCart) {
+      productExistsInCart.quantity += 1;
+    }
+    else {
+      this.items.push({ ...product, quantity: 1 });
+      return;
+    }
+  }
+
+  removeProduct(product: Product) {
+    const productExistsInCart = this.items.find(item => item.id === product.id);
+    if (productExistsInCart) {
+        const index = this.items.indexOf(productExistsInCart);
+        this.items.splice(index, 1);
+    }
+  }
+
+  updateQuantity(product: Product, quantity: number): void {
+    const productExistsInCart = this.items.find(item => item.id === product.id);
+    if (productExistsInCart) {
+      if (quantity > 0) {
+        productExistsInCart.quantity = quantity;
+      }
+      else {
+        const index = this.items.indexOf(productExistsInCart);
+        this.items.splice(index, 1);
+      }
+    }
+  }
+
+  getItems(): Product[] {
+    return this.items;
+  }
+
+  clearCart(): Product[] {
+    this.items = [];
+    return this.items;
+  }
+
+  NumberOfItemsInCart() : number {
+    let numberOfItems = 0;
+    this.items.forEach(item => {
+      numberOfItems += item.quantity;
+    });
+    return numberOfItems;
+  }
+
+  constructor() { }
+}
