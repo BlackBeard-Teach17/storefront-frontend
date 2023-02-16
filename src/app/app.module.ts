abf20a473456565938d523259e8a9e9a387c0dad
Keamo Mfoloe <keamogetswemfoloe@gmail.com>
2023-02-16 14:26:24 +0200

    Feat: Added new dependency primeng

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule} from '@angular/common/http';
 import { Routes, RouterModule } from '@angular/router';
 import { FormsModule, ReactiveFormsModule} from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MessageService } from 'primeng/api';
+import { MessageModule } from 'primeng/message';
+import { ToastModule } from 'primeng/toast';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -11,6 +15,7 @@ import { CartComponent } from './cart/cart.component';
 import { ProductItemDetailComponent } from './product-item-detail/product-item-detail.component';
 import { OrderConfirmationComponent } from './order-confirmation/order-confirmation.component';
 import { HeaderBarComponent } from './header-bar/header-bar.component';
+import { CheckoutComponent } from './checkout/checkout.component';
 
 const routes: Routes = [
   { path: '', component: ProductListComponent },
@@ -25,7 +30,8 @@ const routes: Routes = [
     CartComponent,
     ProductItemDetailComponent,
     OrderConfirmationComponent,
-    HeaderBarComponent
+    HeaderBarComponent,
+    CheckoutComponent
   ],
   imports: [
     BrowserModule,
@@ -33,9 +39,12 @@ const routes: Routes = [
     HttpClientModule,
     RouterModule.forRoot(routes),
     FormsModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    BrowserAnimationsModule,
+    MessageModule,
+    ToastModule
   ],
-  providers: [],
+  providers: [MessageService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
